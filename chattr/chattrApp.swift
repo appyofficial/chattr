e4b665084ca330f802e100728103becec3256268
Aprajit Sharma <appy70890@gmail.com>
2022-06-06 11:21:19 +0200

    Initial Commit

---
+++ chattr/chattrApp.swift
@@ -0,0 +1,17 @@
+//
+//  chattrApp.swift
+//  chattr
+//
+//  Created by Aprajit Sharma on 2022-06-06.
+//
+
+import SwiftUI
+
+@main
+struct chattrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
